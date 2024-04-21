b2bc10f0e0f79fd5d9a7946eed01794cf86ca4ab
Tiago Ferrer <tiago.ferrer@preventsenior.com.br>
2024-04-21 18:54:13 -0300

    init

---
+++ src/test/java/br/com/fiap/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.fiap.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
